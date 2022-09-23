8bf8eb7ee90ef002a0e42e5cc28884b75a1e3909
rvitality <vital.renzjohn@gmail.com>
2022-09-24 02:11:52 +0800

    Add base styling with sass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div>
+            <h1 style={{ fontSize: "7rem" }}>App</h1>
+        </div>
+    );
+};
 
 export default App;
